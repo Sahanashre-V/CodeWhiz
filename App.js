f42bb5ec5acbce8b4ceb1a281911514873fb02b4
Sahanashre-V <sahanashre.v@gmail.com>
2025-03-14 12:30:12 +0530

    Basic setup done

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is my first mobile development..</Text>
+      <Text>This is my first mobile development...</Text>
       <StatusBar style="auto" />
     </View>
   );
