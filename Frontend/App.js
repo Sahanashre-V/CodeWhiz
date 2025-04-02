3f638b4d54908d30015d2cd06d0c169a8c25568d
Sahanashre-V <sahanashre.v@gmail.com>
2025-04-02 11:47:57 +0530

    Added navbar and footer

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Register from './Components/Register';
-import Login from './Components/Login';
-import AIChatScreen from './Components/AIChatScreen';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import AIChatScreen from "./Components/AIChatScreen";
 
 const Stack = createStackNavigator();
 
 const App = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Register">
-                <Stack.Screen name="Register" component={Register} />
+            <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Login" component={Login} />
-                <Stack.Screen name="AI Chat" component={AIChatScreen} />
+                <Stack.Screen name="Register" component={Register} />
+                <Stack.Screen name="AIChatScreen" component={AIChatScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
