0fbc3769801dcf6526b042dc6ce1497977649fc2
Sahanashre-V <sahanashre.v@gmail.com>
2025-04-04 11:37:00 +0530

    Added gemini api

--- Backend/Controllers/AiIntegrate.js
+++ Backend/Controllers/AiIntegrate.js
@@ -1,16 +1,20 @@
+const dotenv = require('dotenv');
+const { GoogleGenAI } = require("@google/genai");
+dotenv.config()
+
 const aiIntegrate = async (req, res) => {
     try {
         const { prompt } = req.body;
 
         if (!prompt) return res.status(400).json({ message: "Prompt is required" });
 
-        const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
+        const response = await ai.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: `${prompt} i am beginer in coding please the code line by line. next take a input and dryrun the code line by line.`,
+          }); 
 
-        const result = await model.generateContent(prompt);
-        const generatedResult = result.response.text();
-
-        return res.status(200).json({ message: generatedResult });
+        return res.status(200).json({ message: response.text });
     } catch (error) {
         console.error("Error generating AI response:", error);
         return res.status(500).json({ message: "Internal server error" });
